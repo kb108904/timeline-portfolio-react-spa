dc8ba32d539c7431e8436995002bb7f989f847a5
kbrownII <kb108904@gmail.com>
2024-08-16 00:50:43 -0400

    minor fix

--- src/components/features/timeline/timeline.test.tsx
+++ src/components/features/timeline/timeline.test.tsx
@@ -3,7 +3,6 @@ import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Timeline from './timeline';
 import { MemoryRouter } from 'react-router-dom';
-
 import { FilesProvider } from '../../../contexts/FIlesContext';
 
 
