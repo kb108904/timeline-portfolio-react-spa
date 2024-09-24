f7b725c6ffe08c3ff0ab06f07d80b95ac91406b7
kbrownII <kb108904@gmail.com>
2024-09-24 01:24:03 -0400

    Update the color refs

--- src/components/pages/project/projectPage.tsx
+++ src/components/pages/project/projectPage.tsx
@@ -35,64 +35,66 @@ export default function ProjectPage(): JSX.Element {
     return (
 
         <div className="parent container bg container-scroll mb-1" data-testid='projectPage'>
-            <div className="col-12 mx-auto">
-                <h1>{selectedItem.pageName}</h1>
-                <MarkdownView content={selectedItem.description} />
-            </div>
-
-            {/* Section for Videos */}
-            {hasVideo && (
-                <div className="row mb-4 justify-content-center">
-                    {selectedItem.videoFiles.map((video) => (
-                        <div key={stringToPseudoUUID(video.fileName)} className={`${selectedItem.videoFiles.length > 1 ? 'col-6' : 'col-12'} mb-4 d-flex align-items-stretch`}>
-                            <div className="card text-center mx-auto d-block w-100">
-                                <div className="video-container">
-                                    <video controls>
-                                        <source src={video.filePath} type="video/mp4" />
-                                        Your browser does not support the video tag.
-                                    </video>
-                                </div>
-                                <div className="card-body">
-                                    <h5 className="card-title">{getFileName(video.fileName)}</h5>
-                                </div>
-                            </div>
-                        </div>
-                    ))}
+            <div className='m-3'>
+                <div className="col-12 mx-auto">
+                    <h1>{selectedItem.pageName}</h1>
+                    <MarkdownView content={selectedItem.description} />
                 </div>
-            )}
 
-            {/* Section for Images */}
-            {hasImages && (
-                <div className="row mb-4">
-                    {selectedItem.imageFiles.map((image) => (
-                        <div key={stringToPseudoUUID(image.fileName)} className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4 d-flex align-items-stretch">
-                            <div className="card text-center mx-auto d-block w-100" onClick={() => { handleImageClick(image.filePath) }}>
-                                <div className="thumbnail-container">
-                                    <img src={getThumbnailPath(image.filePath)} alt={image.fileName} className="thumbnail" />
-                                </div>
-                                <div className="card-body">
-                                    <h5 className="card-title">{getFileName(image.fileName)}</h5>
+                {/* Section for Videos */}
+                {hasVideo && (
+                    <div className="row mb-3 justify-content-center">
+                        {selectedItem.videoFiles.map((video) => (
+                            <div key={stringToPseudoUUID(video.fileName)} className={`${selectedItem.videoFiles.length > 1 ? 'col-6' : 'col-12'} mb-1 d-flex align-items-stretch`}>
+                                <div className="card text-center mx-auto d-block w-100">
+                                    <div className="video-container">
+                                        <video controls>
+                                            <source src={video.filePath} type="video/mp4" />
+                                            Your browser does not support the video tag.
+                                        </video>
+                                    </div>
+                                    <div className="card-body">
+                                        <h5 className="card-title">{getFileName(video.fileName)}</h5>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    ))}
-                </div>
-            )}
-            {showModal && (
-                <div className="modal show d-block" tabIndex={-1} role="dialog" onClick={handleCloseModal} style={{ backgroundColor: 'rgba(0,0,0,0.8)' }}>
-                    <div className="modal-dialog modal-xl modal-dialog-centered" onClick={(e) => { e.stopPropagation(); }} role="document">
-                        <div className="modal-content">
-                            <div className="modal-header">
-                                <h5 className="modal-title">{getFileName(selectedImage)}</h5>
-                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={handleCloseModal}></button>
+                        ))}
+                    </div>
+                )}
+
+                {/* Section for Images */}
+                {hasImages && (
+                    <div className="row mb-4">
+                        {selectedItem.imageFiles.map((image) => (
+                            <div key={stringToPseudoUUID(image.fileName)} className="col-12 col-sm-6 col-md-4 col-lg-3 mb-1 d-flex align-items-stretch">
+                                <div className="card text-center mx-auto d-block w-100" onClick={() => { handleImageClick(image.filePath) }}>
+                                    <div className="thumbnail-container">
+                                        <img src={getThumbnailPath(image.filePath)} alt={image.fileName} className="thumbnail" />
+                                    </div>
+                                    <div className="card-body">
+                                        <h5 className="card-title">{getFileName(image.fileName)}</h5>
+                                    </div>
+                                </div>
                             </div>
-                            <div className="modal-body text-center">
-                                {selectedImage && <img src={selectedImage} alt="Full Size" className="img-fluid" />}
+                        ))}
+                    </div>
+                )}
+                {showModal && (
+                    <div className="modal show d-block" tabIndex={-1} role="dialog" onClick={handleCloseModal} style={{ backgroundColor: 'rgba(0,0,0,0.8)' }}>
+                        <div className="modal-dialog modal-xl modal-dialog-centered" onClick={(e) => { e.stopPropagation(); }} role="document">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <h5 className="modal-title">{getFileName(selectedImage)}</h5>
+                                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={handleCloseModal}></button>
+                                </div>
+                                <div className="modal-body text-center">
+                                    {selectedImage && <img src={selectedImage} alt="Full Size" className="img-fluid" />}
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            )}
+                )}
+            </div>
         </div>
     );
 }
